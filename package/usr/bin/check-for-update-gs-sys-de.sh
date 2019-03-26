76363a566343defb02e333215cbdc4841098fb6d
Georg Schmidt <gs-develop@gs-sys.de>
2019-03-26 12:22:06 +0100

    version 1.1

--- package/usr/bin/check-for-update-gs-sys-de.sh
+++ package/usr/bin/check-for-update-gs-sys-de.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# author: Georg Schmidt <gs-develop@gs-sys.de>
+# version: 03.2019
+
+## location: /usr/bin/check-for-update-gs-sys-de.sh
+# cp check-for-update-gs-sys-de.sh /usr/bin/check-for-update-gs-sys-de.sh
+# chmod 0555 /usr/bin/check-for-update-gs-sys-de.sh
+
 update=`apt list --upgradable`
 
 echo "$update"
@@ -7,7 +16,7 @@ then
 	zenity --question --timeout 120 --width=150 --height=50 --text "Updates should be installed! Now?" --title "New Updates!"
 	case $? in 
 	  0) echo "Yes"
-	     sudo updater
+	     sudo /usr/sbin/updater
 	  ;; 
 	  1) echo "No"
 	     exit 0
@@ -18,4 +27,5 @@ then
 	esac
 else
 	echo "no new updates"
+	exit 0
 fi
