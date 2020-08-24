59d0e1dfadbaec8a1cf7a0ce526104d724cfe077
Georg Schmidt <gs-develop@gs-sys.de>
2020-08-25 00:28:05 +0200

    nespa

---
+++ package/usr/local/bin/javafx.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/bin/java --module-path /usr/share/openjfx/lib --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.web $1 $2 $3 $4 $5 $6 $7 $8
+exit 0
